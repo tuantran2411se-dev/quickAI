55a76c39e4691cdf6d170ac5ce10298040943560
tuantran2411se-dev <tuantran2411.se@gmail.com>
2025-10-15 19:06:30 -0700

    feat: Create Navbar component with logo and "Get Started" button

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="fixed z-50 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
+      <img
+        className="w-32 sm:w-44 cursor-pointer"
+        alt="logo"
+        src={assets.logo}
+        onClick={() => navigate("/")}
+      />
+      <button
+        className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        onClick={() => navigate("/")}
+      >
+        Get started
+        <ArrowRight className="w-4 h-4" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
